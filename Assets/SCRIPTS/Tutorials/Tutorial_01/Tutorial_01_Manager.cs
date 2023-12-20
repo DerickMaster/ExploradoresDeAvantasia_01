6b3009232dd5d9f125ecc2975ae14af63966ec43
João Victor Fernandes Cabral <jgazbral@gmail.com>
2023-12-20 10:44:02 -0300

    #UPDATE - Gelatinoso

--- Assets/SCRIPTS/Tutorials/Tutorial_01/Tutorial_01_Manager.cs
+++ Assets/SCRIPTS/Tutorials/Tutorial_01/Tutorial_01_Manager.cs
@@ -5,15 +5,31 @@ public class Tutorial_01_Manager : MonoBehaviour
 {
     struct TutorialTrigger
     {
-        GameObject Object;
-        string Text, MobileText;
+        public GameObject myObject;
+        public string Text, MobileText;
     }
 
     CanvasBehaviour _canvas;
-    TutorialTrigger[] Triggers;
-    int _tutorialIndex;
+    TutorialTrigger[] _triggers;
+    int _tutorialIndex = -1;
+    [SerializeField] float _textTimer;
     private void Start()
     {
         _canvas = CanvasBehaviour.Instance;
+    }
+
+    public void AdvanceTutorial()
+    {
+        _tutorialIndex += 1;
+        EnableObject();
+    }
+
+    public void EnableObject()
+    {
+        _triggers[_tutorialIndex].myObject.SetActive(true);
+#if UNITY_ANDROID
+        _canvas.SetActiveTempText(_triggers[_tutorialIndex].MobileText, 2f);
+#endif
+        _canvas.SetActiveTempText(_triggers[_tutorialIndex].Text, 2f);
     }
 }

748a5d307cb70e2986a4c3e5b5a14d0c50d3752a
João Victor Fernandes Cabral <jgazbral@gmail.com>
2023-12-20 15:26:20 -0300

    #UPDATE - gelatinoso

--- Assets/SCRIPTS/Tutorials/Tutorial_01/Tutorial_01_Manager.cs
+++ Assets/SCRIPTS/Tutorials/Tutorial_01/Tutorial_01_Manager.cs
@@ -3,33 +3,52 @@ using UnityEngine;
 
 public class Tutorial_01_Manager : MonoBehaviour
 {
+    [System.Serializable]
     struct TutorialTrigger
     {
-        public GameObject myObject;
+        public Tutorial_01_ZoneTrigger myObject;
         public string Text, MobileText;
     }
 
     CanvasBehaviour _canvas;
-    TutorialTrigger[] _triggers;
+    [SerializeField] TutorialTrigger[] _triggers;
     int _tutorialIndex = -1;
     [SerializeField] float _textTimer;
+    [SerializeField] string _gearInstruction, _gearInstructionMobile;
+    [SerializeField] LauraGear _gear;
+    [SerializeField] string _boxInstruction, _boxInstructionMobile;
+    [SerializeField] GameObject _box;
     private void Start()
     {
         _canvas = CanvasBehaviour.Instance;
+        foreach (var item in _triggers)
+        {
+            item.myObject._enteredZoneEvent.AddListener(AdvanceTutorial);
+        }
+        _box.GetComponent<BreakableBox>().unitKilled.AddListener(AdvanceTutorial);
     }
 
     public void AdvanceTutorial()
     {
         _tutorialIndex += 1;
-        EnableObject();
+        ActivateObject();
+        if (_tutorialIndex == _triggers.Length - 1)
+        {
+            _box.SetActive(true);
+            _canvas.SetActiveTempText(_boxInstruction, _textTimer);
+        }else if(_tutorialIndex == _triggers.Length)
+        {
+            _gear.enabled = true;
+            _canvas.SetActiveTempText(_gearInstruction, _textTimer);
+        }
     }
 
-    public void EnableObject()
+    public void ActivateObject()
     {
-        _triggers[_tutorialIndex].myObject.SetActive(true);
+        _triggers[_tutorialIndex].myObject.gameObject.SetActive(true);
 #if UNITY_ANDROID
-        _canvas.SetActiveTempText(_triggers[_tutorialIndex].MobileText, 2f);
+        _canvas.SetActiveTempText(_triggers[_tutorialIndex].MobileText, _textTimer);
 #endif
-        _canvas.SetActiveTempText(_triggers[_tutorialIndex].Text, 2f);
+        _canvas.SetActiveTempText(_triggers[_tutorialIndex].Text, _textTimer);
     }
 }
